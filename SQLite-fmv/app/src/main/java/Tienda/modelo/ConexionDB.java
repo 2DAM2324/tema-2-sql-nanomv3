01da597f72d66d71c9371efdf5b3eb1f2c66b02b
nanomv3 <fernandomar.alumn@cescristorey.com>
2023-11-13 14:06:51 +0100

    Create ConexionDB.java
    He creado la clase ConexionDB

---
+++ SQLite-fmv/app/src/main/java/Tienda/modelo/ConexionDB.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author nanom
+ */
+public class ConexionDB {
+
+    private Connection conexion;
+
+    public ConexionDB() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+            String url = "jdbc:sqlite:tienda.db3";
+            conexion = DriverManager.getConnection(url);
+
+            System.out.println("Conexión exitosa a la base de datos SQLite.");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection obtenerConexion() {
+        return conexion;
+    }
+
+    public void cerrarConexion() {
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
+                System.out.println("Conexión cerrada.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
